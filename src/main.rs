9bc682a148fc2c6556ce7c431bafbb9a209ff047
myyrakle <sssang97@naver.com>
2020-10-11 00:55:51 +0900

    impl test bubble sort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+pub fn bubble_sort(arr: &mut std::vec::Vec<i32>) {
+    let len = arr.len();
+    let mut i = 0;
+
+    while (i + 1) < len {
+        let mut j = i;
+
+        while (j + 1) < len {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut nums = vec![1, 4, 2, 3, 5];
+    bubble_sort(&mut nums);
+    println!("{:?}", nums);
 }
