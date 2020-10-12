157ab86317d6f5170315abbadfbd81788dff21e4
myyrakle <sssang97@naver.com>
2020-10-13 08:19:30 +0900

    impl bubble sort

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,7 @@ pub mod lib;
 pub use lib::*;
 
 fn main() {
-    let mut nums = vec![1, 4, 2, 3, 5, 111, 234, 21, 13];
+    let mut nums = [1, 4, 2, 3, 5, 111, 234, 21, 13];
     bubble_sort_reverse(&mut nums);
-    //println!("{:?}", 1.cmp(&4));
-    // bubble_sort_by(&mut nums, |l, r| match l.cmp(r) {
-    //     std::cmp::Ordering::Greater => std::cmp::Ordering::Less,
-    //     std::cmp::Ordering::Less => std::cmp::Ordering::Greater,
-    //     _ => std::cmp::Ordering::Equal,
-    // });
     println!("{:?}", nums);
 }
--- src/sorts/bubble.rs
+++ src/sorts/bubble.rs
@@ -11,20 +11,7 @@ pub fn bubble_sort_reverse<T>(arr: &mut [T])
 where
     T: std::cmp::Ord,
 {
-    let len = arr.len();
-    let mut i = 0;
-
-    while (i + 1) < len {
-        let mut j = i;
-
-        while (j + 1) < len {
-            if arr[j] < arr[j + 1] {
-                utils::swap(arr, j, j + 1)
-            }
-            j += 1;
-        }
-        i += 1;
-    }
+    bubble_sort_by(arr, |l, r| l.cmp(r).reverse())
 }
 
 pub fn bubble_sort_by<T, F>(arr: &mut [T], compare: F)
@@ -32,20 +19,19 @@ where
     T: std::cmp::Ord,
     F: Fn(&T, &T) -> std::cmp::Ordering,
 {
-    let len = arr.len();
-    let mut i = 0;
+    let mut last = arr.len();
 
-    while (i + 1) < len {
-        let mut j = i;
+    while 0 != last {
+        let mut i = 0;
 
-        while (j + 1) < len {
-            match compare(&arr[j], &arr[j + 1]) {
+        while (i + 1) < last {
+            match compare(&arr[i], &arr[i + 1]) {
                 std::cmp::Ordering::Less => (),
-                std::cmp::Ordering::Greater => utils::swap(arr, j, j + 1),
+                std::cmp::Ordering::Greater => utils::swap(arr, i, i + 1),
                 std::cmp::Ordering::Equal => (),
             }
-            j += 1;
+            i += 1;
         }
-        i += 1;
+        last -= 1;
     }
 }
