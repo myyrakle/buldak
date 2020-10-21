0c7d36325414a3b9991b456570f989147bb3b11c
myyrakle <sssang97@naver.com>
2020-10-22 07:50:16 +0900

    update counting: api change.

--- src/lib/counting.rs
+++ src/lib/counting.rs
@@ -2,6 +2,64 @@
 //!
 //! **O(N)**
 
+/// Sort in ascending order using a counting sort algorithm.
+///
+/// ```rust
+/// use buldak::counting;
+///
+/// let mut nums = [1, 4, 2, 3, 5, -44, 111, 234, 21, 13];
+/// counting::sort(&mut nums);
+/// assert_eq!(nums, [-44, 1, 2, 3, 4, 5, 13, 21, 111, 234]);
+/// ```
+pub fn sort<T>(array: &mut [T]) -> Result<(), String>
+where
+    T: std::convert::TryInto<isize> + std::convert::TryFrom<isize> + std::clone::Clone,
+    <T as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
+    <T as std::convert::TryFrom<isize>>::Error: std::fmt::Debug,
+{
+    _counting_scan_impl(array, true, true)
+}
+
+// pub fn sort<T, Max>(array: &mut [T], max: Max, signed: bool) -> Result<(), String>
+// where
+//     T: std::convert::TryInto<isize> + std::convert::TryFrom<isize> + std::clone::Clone,
+//     <T as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
+//     <T as std::convert::TryFrom<isize>>::Error: std::fmt::Debug,
+//     Max: std::convert::TryInto<isize>,
+//     <Max as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
+// {
+//     _counting_impl(array, max, true, signed)
+// }
+
+/// Sort in descending order using a counting algorithm.
+///
+/// ```rust
+/// use buldak::counting;
+///
+/// let mut nums = [1, 4, 2, 3, 5, 111, 234, 21, 13, -2];
+/// counting::sort_reverse(&mut nums);
+/// assert_eq!(nums, [234, 111, 21, 13, 5, 4, 3, 2, 1, -2]);
+/// ```
+pub fn sort_reverse<T>(array: &mut [T]) -> Result<(), String>
+where
+    T: std::convert::TryInto<isize> + std::convert::TryFrom<isize> + std::clone::Clone,
+    <T as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
+    <T as std::convert::TryFrom<isize>>::Error: std::fmt::Debug,
+{
+    _counting_scan_impl(array, false, true)
+}
+
+// pub fn sort_reverse<T, Max>(array: &mut [T], max: Max, signed: bool) -> Result<(), String>
+// where
+//     T: std::convert::TryInto<isize> + std::convert::TryFrom<isize> + std::clone::Clone,
+//     <T as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
+//     <T as std::convert::TryFrom<isize>>::Error: std::fmt::Debug,
+//     Max: std::convert::TryInto<isize>,
+//     <Max as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
+// {
+//     _counting_impl(array, max, false, signed)
+// }
+
 fn _counting_impl<T, Max>(array: &mut [T], max: Max, asc: bool, signed: bool) -> Result<(), String>
 where
     T: std::convert::TryInto<isize> + std::convert::TryFrom<isize> + std::clone::Clone,
@@ -15,7 +73,7 @@ where
         .expect("Conversion to isize failed. the max value must be convertible to isize.");
     let max = if max >= 0 { max } else { max * -1 };
 
-    let mut pos_count = vec![0; max as usize];
+    let mut pos_count = vec![0; max as usize + 1];
     let mut neg_count = if signed {
         vec![0; max as usize]
     } else {
@@ -39,10 +97,10 @@ where
             let count_index = e as usize;
             pos_count[count_index] += 1;
         } else if signed {
-            let count_index = (e * -1 + 1) as usize;
+            let count_index = (e * -1 - 1) as usize;
             neg_count[count_index] += 1;
         } else {
-            return std::result::Result::Err("Now this Function can only receive natural numbers, but they contain negative numbers.".to_string());
+            return Err("Now this Function can only receive natural numbers, but they contain negative numbers.".to_string());
         }
 
         i += 1;
@@ -95,56 +153,23 @@ where
     return Ok(());
 }
 
-/// Sort in ascending order using a counting sort algorithm.
-///
-/// The parameter 'max' is the absolute maximum value of the received array.
-/// Any elements beyond this value will result in an error.
-///
-/// The parameter 'signed' chooses whether to support negative numbers.
-/// When using only natural numbers, it is twice as efficient to set signed to false.
-/// If signed is set to false and the array contains negative elements, an error occurs.
-///
-/// ```rust
-/// use buldak::counting;
-///
-/// let mut nums = [1, 4, 2, 3, 5, 111, 234, 21, 13];
-/// counting::sort(&mut nums, 300, true);
-/// assert_eq!(nums, [1, 2, 3, 4, 5, 13, 21, 111, 234]);
-/// ```
-pub fn sort<T, Max>(array: &mut [T], max: Max, signed: bool) -> Result<(), String>
+fn _counting_scan_impl<T>(array: &mut [T], asc: bool, signed: bool) -> Result<(), String>
 where
     T: std::convert::TryInto<isize> + std::convert::TryFrom<isize> + std::clone::Clone,
     <T as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
     <T as std::convert::TryFrom<isize>>::Error: std::fmt::Debug,
-    Max: std::convert::TryInto<isize>,
-    <Max as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
 {
-    _counting_impl(array, max, true, signed)
-}
+    if array.len() == 0 {
+        return Ok(());
+    } 
 
-/// Sort in descending order using a counting algorithm.
-///
-/// The parameter 'max' is the absolute maximum value of the received array.
-/// Any elements beyond this value will result in an error.
-///
-/// The parameter 'signed' chooses whether to support negative numbers.
-/// When using only natural numbers, it is twice as efficient to set signed to false.
-/// If signed is set to false and the array contains negative elements, an error occurs.
-///
-/// ```compile_fail
-/// use buldak::counting;
-///
-/// let mut nums = [1, 4, 2, 3, 5, 111, 234, 21, 13];
-/// counting::sort_reverse(&mut nums);
-/// assert_eq!(nums, [234, 111, 21, 13, 5, 4, 3, 2, 1]);
-/// ```
-pub fn sort_reverse<T, Max>(array: &mut [T], max: Max, signed: bool) -> Result<(), String>
-where
-    T: std::convert::TryInto<isize> + std::convert::TryFrom<isize> + std::clone::Clone,
-    <T as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
-    <T as std::convert::TryFrom<isize>>::Error: std::fmt::Debug,
-    Max: std::convert::TryInto<isize>,
-    <Max as std::convert::TryInto<isize>>::Error: std::fmt::Debug,
-{
-    _counting_impl(array, max, false, signed)
+    let mut abs_max:isize = array[0].to_owned().try_into().unwrap().abs();
+    for e in array.iter() {
+        let e:isize = e.to_owned().try_into().unwrap().abs();
+        if e > abs_max {
+            abs_max=e;
+        }
+    }
+
+    return _counting_impl(array, abs_max, asc, signed);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod lib;
 pub use lib::*;
 
 fn main() {
-    let mut nums = [1, 4, 2, 3, 5, 111, 234, 21, 13];
-    comb::sort_reverse(&mut nums);
-    println!("{:?}", nums);
+    let mut nums = [1, 4, 2, 3, 5, 111, 234, -234, -33, -32, 21, 13];
+    //counting::sort_reverse(&mut nums);
+    //println!("{:?}", nums);
 }
